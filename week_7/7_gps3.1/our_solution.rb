30faef551af75714cea18afbd35d5feba53ff6be
Maria Piper <maria@devbootcamp.com>
2014-04-20 12:24:24 -0700

    added GPS challenge

---
+++ week_7/7_gps3.1/our_solution.rb
@@ -0,0 +1,34 @@
+# U3.W7: Build an Electronic Grocery List!
+ 
+# Your full names:
+# 1.
+# 2.
+ 
+# User Stories (As many as you want. Delete the statements you don't need)
+# As a user, I want to
+# As a user, I want to
+# As a user, I want to
+# As a user, I want to
+# As a user, I want to
+ 
+# Pseudocode
+# 
+# 
+# 
+# 
+# 
+# 
+ 
+ 
+# Your fabulous code goes here....
+
+
+
+
+
+
+
+# DRIVER CODE GOES HERE. 
+ 
+ 
+ 
