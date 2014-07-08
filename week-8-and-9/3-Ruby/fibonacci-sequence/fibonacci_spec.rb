c62da2dcca86b121bc1847322ad5f125e73ce152
jessonfoo <jessonfoo@gmail.com>
2014-07-08 13:09:05 -0700

    update

--- week-8-and-9/3-Ruby/fibonacci-sequence/fibonacci_spec.rb
+++ week-8-and-9/3-Ruby/fibonacci-sequence/fibonacci_spec.rb
@@ -6,15 +6,15 @@ end
 
 describe "is_fibonacci?" do
    it "returns true when a integer is one number of the Fibonacci sequence" do
-      is_fibonacci?(random_fibonacci).should be_true
+      is_fibonacci?(random_fibonacci).should be_truthy
    end
    it "returns true with a large Fibonacci number" do
-      is_fibonacci?(8670007398507948658051921).should be_true
+      is_fibonacci?(8670007398507948658051921).should be_truthy
    end
    it "returns false when it's not in the Fibonacci seqence" do
-      is_fibonacci?(random_fibonacci+100).should be_false
+      is_fibonacci?(random_fibonacci+100).should be_falsy
    end
    it "returns false on large non-Fibonacci numbers" do
-      is_fibonacci?(927372692193078999171).should be_false
+      is_fibonacci?(927372692193078999171).should be_falsy
    end
 end
--- week-8-and-9/3-Ruby/fibonacci-sequence/my_solution.rb
+++ week-8-and-9/3-Ruby/fibonacci-sequence/my_solution.rb
@@ -1,32 +1,59 @@
+require_relative "../../assert.rb"
 # U3.W8-9: 
 
 
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
-
+# create a way to add the last two numbers of an array together
+# create a loop that stores a variable containing the next fibonacci number and add that number to the end of the array
+# exit the loop when the last number in the array is greater than the argument
+# check if the argument is included in the array
 
 
 # 3. Initial Solution
 
-def is_fibonacci?(num)
+# def is_fibonacci?(num)
+# 	fib = [0,1]
+# 	while fib.last < num
+# 		next_fib = fib[-1] + fib[-2]
+# 		fib << next_fib
+# 	end
+# 	fib.include? num
+# end
 
-end
+
 
 
 
 # 4. Refactored Solution
 
 
+def is_fibonacci?(num)
+   fib = [0,1]
+   while num > fib.last
+      next_fib = fib[-1] + fib[-2]
+      fib << next_fib
+   end
+   fib.include? num
+end
+
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
+def random_fibonacci
+   [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946].sample
+end
+assert(1){is_fibonacci?(random_fibonacci) == true}
+assert(2){is_fibonacci?(8670007398507948658051921) == true}
+assert(3){is_fibonacci?(random_fibonacci+100) == false}
+assert(4){is_fibonacci?(927372692193078999171) == false}
 
 
 
 # 5. Reflection 
+# This challenge caused a lot of trouble because the rspec tests were not passing. I was stuck on this problem
+# for a good deal of time because the rspec needed to be changed. I felt like it was a waste of time since 
+# my translated driver tests worked, but the only thing that caused me to lag on this problem was the rspec tests.
