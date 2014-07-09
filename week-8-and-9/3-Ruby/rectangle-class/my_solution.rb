e2a1150627eb737fe1fac5e4b659361331987990
jessonfoo <jessonfoo@gmail.com>
2014-07-08 20:39:19 -0700

    update

--- week-8-and-9/3-Ruby/rectangle-class/my_solution.rb
+++ week-8-and-9/3-Ruby/rectangle-class/my_solution.rb
@@ -8,8 +8,40 @@
 
 
 # 3. Initial Solution
+# class Rectangle
+#   attr_accessor :width, :height, :area
+
+#   def initialize(width, height)
+#     @width  = width
+#     @height = height
+#   end
+
+#   def area
+#   	@area = @width*@height
+#   	@area
+#   end
+#   def perimeter
+#   	@perimeter = 2*(@width+@height)
+#   end
+#   def diagonal
+#   	Math.hypot(@width,@height)
+#   end
+#   def square?
+#   	return true if @width == @height
+#   end
+
+#   def ==(other)
+#     (other.width  == self.width && other.height == self.height ) ||
+#     (other.height == self.width && other.width  == self.height )
+#   end
+# end
+
+
+
+
+# 4. Refactored Solution
 class Rectangle
-  attr_accessor :width, :height, :area
+  attr_accessor :width, :height
 
   def initialize(width, height)
     @width  = width
@@ -17,40 +49,36 @@ class Rectangle
   end
 
   def area
-  	@area = @width*@height
-  	@area
+    @area = @width*@height
   end
   def perimeter
-  	@perimeter = 2*(@width+@height)
+    @perimeter = 2*(@width+@height)
   end
   def diagonal
-  	Math.hypot(@width,@height)
+    Math.hypot(@width,@height)
   end
   def square?
-  	return true if @width == @height
-  end
-
-  def ==(other)
-    (other.width  == self.width && other.height == self.height ) ||
-    (other.height == self.width && other.width  == self.height )
+    @width == @height
   end
 end
 
 
-
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+rec = Rectangle.new(10,20)
+square = Rectangle.new(20,20)
+p rec.area == 200
+p rec.perimeter == 60
+p rec.diagonal == 22.360679774997898
 
-
-
-
-
+p rec.area == 200
+p square.area == 400
+p rec.perimeter == 60
+p square.perimeter == 80
+p rec.diagonal == 22.360679774997898
+p square.diagonal == 28.284271247461902
+p rec.square? == false
+p square.square? == true
 
 # 5. Reflection 
+# this challenge was very straight forward. defining each method was very simple. The only thing that was new was 
+# incorporating the Math module. I thought it was a fairly easy challenge compared to some of the other ones.
